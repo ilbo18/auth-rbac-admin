88e51efb49783b3e546fa61d0516a1428631d37f
seungha <songsh@bellock.co.kr>
2026-03-31 16:31:40 +0900

    initialize spring boot project

---
+++ src/main/java/io/github/ilbo18/authrbac/AuthRbacAdminApplication.java
@@ -0,0 +1,13 @@
+package io.github.ilbo18.authrbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRbacAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthRbacAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ilbo18/authrbac/AuthRbacAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ilbo18.authrbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthRbacAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
